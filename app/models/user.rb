4d36631f97c505d1d78cf0cfdeeb7661c4eacc1d
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-04-09 16:57:37 -0400

    relationship with contacts added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :contacts
   validates :email, email: true, uniqueness: true, length: { maximum: 25 }
 end
