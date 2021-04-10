b1ad37229c7526970ebc77f580f27a49a2f4bcc2
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-04-09 23:58:37 -0400

    contacts routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   get 'contacts/index'
+  resources :contacts do
+    collection { post :import }
+  end
   root 'home#index'
 end
