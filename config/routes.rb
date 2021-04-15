a5dfef73c28534d85017b95bd74a922cb6cb8856
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-04-15 05:07:25 -0400

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :users
-  get 'contacts/index'
-  resources :contacts do
+  resources :contacts
+  resources :csv_imports do
     collection { post :import }
   end
+  resources :invalid_records, only: [:index]
   root 'home#index'
 end
